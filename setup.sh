6850f6d9e2809578ba39acba307bc35a5142278d
Andrew Naoum <andrew@naoum.com>
2020-03-31 09:13:13 +1100

    update setup script

--- setup.sh
+++ setup.sh
@@ -6,6 +6,8 @@ git clone --bare https://github.com/anaoum/dotfiles-bare.git .git
 cd .git
 git config core.bare false
 git config core.sparseCheckout true
+git config status.showUntrackedFiles no
+git config bash.showUntrackedFiles false
 
 cat <<EOF > info/sparse-checkout
 /*
